7d6ea77671bd818127333452a3d7e1fbb964193b
Elvis <s49188705@gmail.com>
2022-08-26 17:18:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word){
+  // abc => cba
+  // const wordArray = word.split('')
+  // const reversedWordArray = wordArray.reverse()
+  // const reversedWord = reversedWordArray.join('')
+  // return reversedWord
+  return word.split('').reverse('').join('')
 }
 
+
+
+function isPalindrome(word) {
+  const wordReversed = reverse(word)
+  return word === wordReversed
+  }
+
+
+//Tests cases
+// mom === mom // true
+// abc === cba // false
 /* 
   Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true
+  
+  1.Reverse the string
+  2.check if the input is the same as the reversed input, return true
+    else
+    return false
+
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,6 +39,21 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 
   console.log("");
 
